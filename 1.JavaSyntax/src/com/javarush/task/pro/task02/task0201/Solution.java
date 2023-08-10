1fa4060440ca1dc2ea932c8d2b5a33b721a5748e
setka <setka1748@mail.ru>
2023-08-10 16:42:40 +0500

    javaSyntax task0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+       int intValue = 3, numericValue = 4, size = 6, number = 1;
+
+    }
+}
