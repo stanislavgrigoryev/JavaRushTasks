ce8429b2ff3e4d7a01b2c670e21d268db3386b84
setka <setka1748@mail.ru>
2023-08-10 14:50:57 +0500

    javaSyntax task0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int a = 3126 - 8;
+        System.out.println(a);
+    }
+}
