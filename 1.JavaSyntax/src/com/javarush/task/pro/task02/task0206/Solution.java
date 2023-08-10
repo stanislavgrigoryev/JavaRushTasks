aa6256d4a1f12c5cf995ef1be3b3d15e93801142
setka <setka1748@mail.ru>
2023-08-10 17:12:58 +0500

    javaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    String  word = "world", phrase = "phrase", line = "line",  text = "text";
+    }
+}
