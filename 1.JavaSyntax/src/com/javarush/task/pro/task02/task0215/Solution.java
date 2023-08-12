6b4a769d5d28fb3edda76d1a78d75fe7e7b20315
setka <setka1748@mail.ru>
2023-08-12 20:40:04 +0500

    javaSyntax task0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x = 999;
+        x++;
+       
+        System.out.println(x);
+
+    }
+}
