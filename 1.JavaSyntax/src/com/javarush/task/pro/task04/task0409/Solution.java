f2855761ee36f0b27d47ef30370c0e653af4a996
setka <setka1748@mail.ru>
2023-08-13 17:12:50 +0500

    javaSyntax task0410

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task04.task0409;
+
+import java.util.Scanner;
+
+/* 
+Минимум из введенных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task04.task0410;
+
+import java.util.Scanner;
+
+/* 
+Второе минимальное число из введенных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = scanner.nextInt();
+        int secondMin = scanner.nextInt();
+        if (min > secondMin) {
+            int tmp = secondMin;
+            secondMin = min;
+            min = tmp;
+        }
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            if (min == secondMin && number > min){
+                secondMin = number;
+            }else if (number < min){
+                secondMin = min;
+                min = number;
+            } else if (min < number && number < secondMin) {
+                secondMin = number;
+            }
+        }
+        System.out.println(secondMin);
+    }
+}
