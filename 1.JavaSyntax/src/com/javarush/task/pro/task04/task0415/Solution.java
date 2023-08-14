08067a1d865c63f02247d205a8a20e99d98a2292
setka <setka1748@mail.ru>
2023-08-14 15:38:12 +0500

    javaSyntax task0415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0415;
+
+import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double S = 3.14 * radius * radius;
+        int result = (int) S;
+
+        System.out.println(result);
+
+
+    }
+}
