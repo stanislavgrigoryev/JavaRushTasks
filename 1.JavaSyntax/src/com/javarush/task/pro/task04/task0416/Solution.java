d616432c2635afa70476ad6a92a0c829b6f364af
setka <setka1748@mail.ru>
2023-08-14 15:04:11 +0500

    javaSyntax task0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        double result = number1 * 1.0 / number2;
+        System.out.println(result);
+
+    }
+}
