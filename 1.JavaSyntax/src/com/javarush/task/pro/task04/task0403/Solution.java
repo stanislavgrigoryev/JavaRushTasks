f24a5f311f25a13adcf68500253542deb00a4b00
setka <setka1748@mail.ru>
2023-08-16 16:16:17 +0500

    javaSyntax task0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -8,15 +8,21 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       String text = scanner.nextLine();
-       int sum = 0;
-        if (text.equals("ENTER")){
-            while (scanner.hasNextInt()){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
                 int number = scanner.nextInt();
-                sum*=number;
-                System.out.println(sum); 
+                sum = sum + number;
+            } else if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("ENTER")) {
+                    break;
+                }
+            }
+
         }
-       }
+        System.out.println(sum);
     }
+
 }
