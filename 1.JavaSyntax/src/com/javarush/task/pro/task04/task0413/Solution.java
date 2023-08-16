242a331bc708d2cc1024d9d9b3dd30b6b5716971
setka <setka1748@mail.ru>
2023-08-16 16:19:17 +0500

    javaSyntax task0413

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0413;
+
+/* 
+Рисуем треугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0 ; j <= i; j++) {
+                System.out.print('8');
+            }
+            System.out.println();
+
+        }
+
+    }
+}
