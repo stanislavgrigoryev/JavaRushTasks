cf276e3458b79ffd94ea85d5a10e5b4b25b5d940
setka <setka1748@mail.ru>
2023-08-18 14:44:28 +0500

    javaSyntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(String b){
+
+    }
+    public static void universalMethod(String b, int a){
+
+    }
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int a, String b){
+
+    }
+    public static void universalMethod(double b){
+
+    }
+    public static void universalMethod(float a){
+
+    }
+    public static void universalMethod(float a, String b){
+
+    }
+    public static void universalMethod(double c, int a){
+
+    }
+    public static void universalMethod(float a, double b){
+
+    }
+
+}
