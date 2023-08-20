a35f630559fa56aeb44da25d81ff4e18ca442da5
setka <setka1748@mail.ru>
2023-08-20 17:09:24 +0500

    javaSyntax task0507

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.pro.task05.task0507;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -10,6 +12,20 @@ public class Solution {
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+       Scanner scanner = new Scanner(System.in);
+       int n = scanner.nextInt();
+       array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+//        int max = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (max < array[i]){
+//                max = array[i];
+//            }
+//        }
+//        System.out.println(max);
+        Arrays.sort(array);
+        System.out.println(array[array.length - 1]);
     }
 }
