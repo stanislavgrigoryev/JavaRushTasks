7efc8001be597a48df628799338f1a476a8bf302
setka <setka1748@mail.ru>
2023-08-22 11:41:48 +0500

    javaSyntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    boolean aBoolean;
+    char aChar;
+}
