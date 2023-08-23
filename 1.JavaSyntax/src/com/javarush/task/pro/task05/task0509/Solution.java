4d2c98008b4517a65604002bc37921a48cf08892
setka <setka1748@mail.ru>
2023-08-23 15:09:11 +0500

    javaSyntax task0509

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -12,13 +12,12 @@ public class Solution {
 
     public static void main(String[] args) {
         MULTIPLICATION_TABLE = new int[10][10];
-        for (int i = 1; i <=10; i++) {
+        for (int i = 1; i <= 10 ; i++) {
             for (int j = 1; j <= 10; j++) {
                 MULTIPLICATION_TABLE[i - 1][j - 1] = j * i;
-                System.out.print(MULTIPLICATION_TABLE[i-1][j-1] + " ");
+                System.out.print(MULTIPLICATION_TABLE[i - 1][j-1] + " ");
             }
             System.out.println();
-
         }
     }
 }
