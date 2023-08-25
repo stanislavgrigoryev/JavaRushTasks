54f2b82c2243e9b27f064f5da3b6869f80f4cd76
setka <setka1748@mail.ru>
2023-08-25 16:25:05 +0500

    javaSyntax task0919

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0919/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task09.task0919;
+
+/* 
+Разворот строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Ходит кот задом наперед";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+       StringBuilder stringBuilder = new StringBuilder(string);
+       stringBuilder.reverse();
+        return stringBuilder.toString();
+    }
+}
