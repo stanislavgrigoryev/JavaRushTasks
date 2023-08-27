d203e667073df48db9799a2898a351792bb5b18b
setka <setka1748@mail.ru>
2023-08-27 22:24:18 +0500

    javaSyntax task1019

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1019/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task10.task1019;
+
+import java.util.Calendar;
+
+/* 
+Текущий год
+*/
+
+public class Solution {
+
+    private final int currentYear;
+
+    public Solution() {
+
+        this.currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public int getCurrentYear() {
+
+        return currentYear;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.getCurrentYear());
+    }
+}
