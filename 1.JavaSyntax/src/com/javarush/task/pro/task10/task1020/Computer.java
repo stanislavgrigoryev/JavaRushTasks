c3c15cb2d9f95c0f690d0a4ee5a7d81929109d52
setka <setka1748@mail.ru>
2023-08-27 22:41:12 +0500

    javaSyntax task1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer() {
+       Keyboard keyboard1 = new Keyboard();
+       Monitor monitor1 = new Monitor();
+      Mouse mouse1 = new Mouse();
+      SystemUnit systemUnit1 =  new SystemUnit();
+    }
+}
