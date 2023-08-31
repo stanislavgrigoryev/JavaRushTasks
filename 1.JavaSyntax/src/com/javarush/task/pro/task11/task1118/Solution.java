788470248acb84b09afa11502a8d467e06584d0d
setka <setka1748@mail.ru>
2023-08-31 15:12:12 +0500

    javaSyntax task1118

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1118/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1118;
+
+/* 
+Импортный код
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        java.util.Scanner console = new java.util.Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
