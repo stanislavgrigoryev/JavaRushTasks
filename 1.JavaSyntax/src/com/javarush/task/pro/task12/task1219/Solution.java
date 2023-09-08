af4739dde700095ac1bc2d447301c0d26a83c750
setka <setka1748@mail.ru>
2023-09-08 14:57:43 +0500

    javaSyntax task1219

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Solution.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Solution {
 
     public static void main(String[] args) {
-        var strings = new ArrayList<String>()
+        ArrayList<String> strings = new ArrayList<String>()
         {{
                 add("Так");
                 add("тоже");
