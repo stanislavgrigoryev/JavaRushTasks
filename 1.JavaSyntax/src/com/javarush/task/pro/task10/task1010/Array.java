e95a2dc8920910319fe50e37311d4294d639545d
setka <setka1748@mail.ru>
2023-09-09 14:41:09 +0500

    javaSyntax taskArray

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Array.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+
+//public class Array {
+//    ArrayList<Cat> cats = new ArrayList<>();
+//    Cat thomas = new Cat("Томас");
+//    Cat behemoth = new Cat("Бегемот");
+//    Cat philipp = new Cat("Филипп Маркович");
+//    Cat pushok = new Cat("Пушок");
+//
+//   cats.add(thomas);
+//   cats.add(behemoth);
+//   cats.add(philipp);
+//   cats.add(pushok);
+//   System.out.println(cats.toString());
+//
+//   cats.remove(philipp);
+//
+//   System.out.println(cats.toString());
+//}
+//
