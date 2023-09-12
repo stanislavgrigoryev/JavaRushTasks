1dcfc467029a1311b364a7b7b84a564a5b903a13
setka <setka1748@mail.ru>
2023-09-12 20:53:21 +0500

    javaSyntax task1308

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/UniversityDepartment.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/UniversityDepartment.java
@@ -6,19 +6,20 @@ import java.util.ArrayList;
 Дженерики и студенты
 */
 
-public class UniversityDepartment {
-
-    public static void main(String[] args) {
-        var students = new ArrayList<Student>();
-        students.add(new Student("Акакий"));
-        students.add(new Student("Любослав"));
-
-        printStudentNames(students);
-    }
-
-    public static void printStudentNames(ArrayList<Student> students) {
-        for (int i = 0; i < students.size(); i++) {
-            System.out.println(students.get(i).getName());
-        }
-    }
-}
+//public class UniversityDepartment {
+//
+//    public static void main(String[] args) {
+//        var students = new ArrayList<Student>();
+//        students.add(new Student("Акакий"));
+//        students.add(new Student("Любослав"));
+//
+//        printStudentNames(students);
+//    }
+//
+//    public static void printStudentNames(ArrayList<Student> students) {
+//        for (int i = 0; i < students.size(); i++) {
+//            System.out.println(students.get(i).getName());
+//        }
+//    }
+//}
+//
