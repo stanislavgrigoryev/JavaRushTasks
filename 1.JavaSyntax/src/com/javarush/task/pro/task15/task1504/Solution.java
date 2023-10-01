e22fe11b3c1f6c62d100eb3ff9a07fc7981bbbe8
setka <setka1748@mail.ru>
2023-10-01 16:40:58 +0500

    JavaRushSyntax task1504

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -12,8 +13,20 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        //напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        try (InputStream inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
+             OutputStream outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))) {
+            byte[] bytes = inputStream.readAllBytes();
+            for (int i = 0; i < bytes.length; i+=2) {
+                if (i < bytes.length - 1) {
+                    byte temp = bytes[i];
+                    bytes[i] = bytes[i + 1];
+                    bytes[i + 1] = temp;
+                }
+            }
+            outputStream.write(bytes);
+        }
     }
 }
 
