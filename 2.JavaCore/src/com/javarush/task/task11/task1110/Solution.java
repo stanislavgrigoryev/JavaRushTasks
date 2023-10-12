674bd98a249c9d8ea3efccc0a7277c4a93c8748a
setka <setka1748@mail.ru>
2023-10-12 16:13:31 +0500

    JavaRushCore task1110

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task11.task1110;
+
+/* 
+Не забываем инкапсулировать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight, int speed) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.speed = speed;
+
+        }
+
+        public void setName(String name) {
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setAge(int age) {
+
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+        public int getWeight(){
+            return weight;
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+        public int getSpeed(){
+            return speed;
+        }
+    }
+}
