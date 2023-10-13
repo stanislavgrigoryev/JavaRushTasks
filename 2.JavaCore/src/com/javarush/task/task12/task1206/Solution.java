660efc9ed9a264a7ffd08cd787391bffc96bdcdf
setka <setka1748@mail.ru>
2023-10-13 23:10:45 +0500

    JavaRushCore task1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static int print(int a){
+        return 2;
+    }
+    public static String print(String a){
+        return "";
+    }
+}
