77bdfadce62a6252bbd97e427f7e2221bdc92389
setka <setka1748@mail.ru>
2023-10-15 19:58:04 +0500

    JavaRushCore task1319

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String destinationFileName = reader.readLine();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFileName));
+
+        StringBuilder builder = new StringBuilder();
+
+        String s = "";
+        while (!s.equals("exit")) {
+            s = reader.readLine();
+            builder.append(s).append("\n");
+        }
+
+        writer.write(builder.toString());
+        writer.close();
+    }
+}
