4a79a4272ca800c5f245e775cd6c9df5a15c350e
setka <setka1748@mail.ru>
2023-10-17 23:32:18 +0500

    JavaRushCore task1418

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task14.task1418;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Исправь четыре ошибки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Number> list = new LinkedList<Number>();
+        initList(list);
+        printListValues(list);
+        processCastedObjects(list);
+    }
+
+    private static void processCastedObjects(List<Number> list) {
+        for (Number object : list) {
+            if (object instanceof Float) {
+                Float a = (Float) object;
+                System.out.println("Is float value defined? " + !(a.isNaN()));
+            } else if (object instanceof Double) {
+                Double a = (Double) object;
+                System.out.println("Is double value infinite? " + a.isInfinite());
+            }
+        }
+    }
+
+    private static void printListValues(List<Number> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    private static void initList(List<Number> list) {
+        list.add(1000f);
+        list.add(-90 / -3);
+
+    }
+}
