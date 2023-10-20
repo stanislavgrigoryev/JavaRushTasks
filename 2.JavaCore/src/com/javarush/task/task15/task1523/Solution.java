3b9d60763ab3a8517af597fad3a7746c60ec5762
setka <setka1748@mail.ru>
2023-10-20 23:37:48 +0500

    JavaRushCore task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(int a) {
+
+    }
+
+    protected Solution(float b) {
+
+    }
+
+    private Solution(double d) {
+
+    }
+
+    Solution(String b) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int a) {
+        super(a);
+    }
+
+    protected SubSolution(float b) {
+        super(b);
+    }
+
+    SubSolution(String b) {
+        super(b);
+    }
+
+//    public SubSolution(int a, int b) {
+//    }
+//    SubSolution(float b, float c){
+//
+//    }
+//    protected SubSolution(double a, double c){
+//
+//    }
+}
