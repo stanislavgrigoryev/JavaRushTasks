3be72261954db1faa7a0d26f76f18da32b8845cc
setka <setka1748@mail.ru>
2023-10-21 20:46:37 +0500

    JavaRushCore task1525

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        File file = new File(Statics.FILE_NAME);
+        String string1 = null;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] strings = line.split(",");
+                for (String string : strings) {
+                    lines.add(string);
+                }
+            }
+        } catch (FileNotFoundException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
