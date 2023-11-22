3c9f528a09cdc55cc8562961f2e8b386fa79817b
setka <setka1748@mail.ru>
2023-11-22 17:29:29 +0500

    JavaRushCore task3405

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Solution.java
@@ -1,9 +1,14 @@
 package com.javarush.task.jdk13.task34.task3405;
 
+import lombok.SneakyThrows;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /* 
 Нарушение приватности
@@ -20,12 +25,29 @@ public class Solution {
         print(getFields(task));
     }
 
-    public static Map<String, Object> getFields(Object object) throws Exception {
+    @SneakyThrows
+    private static Object fieldGetObject(Field field, Object object) {
+        return field.get(object);
+    }
+
+    public static Map<String, Object> getFields(Object object) {
+
+       return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(field -> Modifier.isPrivate(field.getModifiers()))
+                .peek(field -> field.setAccessible(true))
+                .collect(Collectors.toMap(
+                        field -> field.getName(),
+                        field -> fieldGetObject(field,object),
+                        (oldValue,newValue)->newValue
+                ));
+    }
+
+    public static Map<String, Object> getFields1(Object object) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        for (Field field:object.getClass().getDeclaredFields()) {
-            if (Modifier.isPrivate(field.getModifiers())){
+        for (Field field : object.getClass().getDeclaredFields()) {
+            if (Modifier.isPrivate(field.getModifiers())) {
                 field.setAccessible(true);
-                map.put(field.getName(),field.get(object));
+                map.put(field.getName(), field.get(object));
             }
         }
         return map;
