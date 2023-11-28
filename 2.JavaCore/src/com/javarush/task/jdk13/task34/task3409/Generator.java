7abf5bae02f48d8669f09f61a24857df8e33ab40
setka <setka1748@mail.ru>
2023-11-28 16:20:02 +0500

    JavaRushCore task3409

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3409/Generator.java
@@ -0,0 +1,18 @@
+package com.javarush.task.jdk13.task34.task3409;
+
+
+import java.lang.reflect.Constructor;
+
+public class Generator<T> {
+
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass) {
+        this.aClass = aClass;
+    }
+
+    T newInstance() throws Exception {
+        Constructor<T> declaredConstructors = (Constructor<T>) aClass.getDeclaredConstructors()[0];
+        return declaredConstructors.newInstance();
+    }
+}
