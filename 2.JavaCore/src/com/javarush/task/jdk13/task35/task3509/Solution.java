f18abcb686e933acd17ed0c915ece718a7d44f0b
setka <setka1748@mail.ru>
2023-12-06 18:07:05 +0500

    JavaRushCore task3509

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.jdk13.task35.task3509;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/* 
+Wildcards для коллекций
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<Double> doubles = new ArrayList<>();
+        doubles.add(11.0);
+        doubles.add(1.1);
+        System.out.println(sum(doubles));
+    }
+
+    public static Double sum(List<? extends Number> list) {
+        Double result = 0.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result += numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static Double multiply(List<? extends Number> list) {
+        Double result = 1.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result *= numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static String concat(List<? extends Object> list) {
+        StringBuilder builder = new StringBuilder();
+        for (Object obj : list) {
+            builder.append(obj);
+        }
+        return builder.toString();
+    }
+
+    public static List combine(List<? extends Collection> list) {
+        List result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            Collection collection = (Collection) list.get(i);
+            result.addAll(collection);
+        }
+        return result;
+    }
+}
