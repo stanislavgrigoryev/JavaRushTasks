48cead9f4a2752d731895a5a09e468d5a7f4ad18
setka <setka1748@mail.ru>
2023-12-08 14:49:08 +0500

    JavaRushCore task1509

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1509/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task15.task1509;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
+
+/* 
+Еще раз читаем из консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String line = scanner.nextLine();
+            char[] chars = line.toCharArray();
+            Set<Character> characters = new HashSet<>();
+            for (char aChar : chars) {
+                if (Character.isAlphabetic(aChar)) {
+                    characters.add(Character.toLowerCase(aChar));
+                }
+            }
+            System.out.println(characters);
+        }
+    }
+}
+
