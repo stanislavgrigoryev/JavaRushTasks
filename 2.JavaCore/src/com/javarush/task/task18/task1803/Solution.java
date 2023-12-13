cb31ab12826c4beb3ad0c72a654b4a5601790b05
setka <setka1748@mail.ru>
2023-12-13 17:33:44 +0500

    JavaRushCore task1803

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /* 
 Самые частые байты
@@ -12,24 +17,29 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        try (FileInputStream fileInputStream = new FileInputStream(scanner.nextLine())) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-            while (fileInputStream.available() > 0) {
-                int read = fileInputStream.read();
-                if (!map.containsKey(read)) {
-                    map.put(read, 1);
-                } else {
-                    map.put(read, map.get(read)+1);
-                }
-            }
-            Integer max = Collections.max(map.values());
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (entry.getValue().equals(max)){
-                    Integer key = entry.getKey();
-                    System.out.print(key + " ");
-                }
-            }
-        }
+
+
+        byte[] bytes = Files.readAllBytes(Path.of(new Scanner(System.in).nextLine()));
+        HashMap<Byte, Integer> map = IntStream.range(0, bytes.length).boxed().collect(Collectors.toMap(index -> bytes[index], integer -> 1, Integer::sum, HashMap::new));
+        map.entrySet().stream().filter(e -> Objects.equals(e.getValue(),Collections.max(map.values()))).forEach(e-> System.out.print(e.getKey() + " "));
+//        Scanner scanner = new Scanner(System.in);
+//        try (FileInputStream fileInputStream = new FileInputStream(scanner.nextLine())) {
+//            HashMap<Integer, Integer> map = new HashMap<>();
+//            while (fileInputStream.available() > 0) {
+//                int read = fileInputStream.read();
+//                if (!map.containsKey(read)) {
+//                    map.put(read, 1);
+//                } else {
+//                    map.put(read, map.get(read)+1);
+//                }
+//            }
+//            Integer max = Collections.max(map.values());
+//            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//                if (entry.getValue().equals(max)){
+//                    Integer key = entry.getKey();
+//                    System.out.print(key + " ");
+//                }
+//            }
+//        }
     }
 }
