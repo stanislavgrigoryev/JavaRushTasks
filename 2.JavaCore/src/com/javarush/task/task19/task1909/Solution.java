98cc5844049af7dea2ae59a4670c9b49cddb0698
setka <setka1748@mail.ru>
2024-01-04 13:27:20 +0500

    JavaRushCore task1909

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task19.task1909;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Замена знаков
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file1));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file2))) {
+            while (bufferedReader.ready()) {
+                String text = bufferedReader.readLine();
+                String string = text.replaceAll("\\.", "!");
+                bufferedWriter.write(string);
+            }
+        }
+        reader.close();
+    }
+}
