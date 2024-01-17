6d93a432f7fe9bf54f7b6e1463a9e95b9e3b641d
setka <setka1748@mail.ru>
2024-01-17 16:46:58 +0500

    JavaRushCore task1920

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -3,34 +3,47 @@ package com.javarush.task.task19.task1920;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /* 
 Самый богатый
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
-            TreeMap<String,Double> map = new TreeMap<>();
-            while (reader.ready()){
-                String text = reader.readLine();
-                String[] split = text.split(" ");
-                String name = split[0];
+    public static void main(String[] args) throws IOException {
+        Map<String, Double> map = new TreeMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]))) {
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                String[] split = line.split(" ");
+                String key = split[0];
                 Double value = Double.parseDouble(split[1]);
+                map.merge(key, value, Double::sum);
 
-                if (!map.containsKey(name)) {
-                    map.put(name,value);
-                } else {
-                    map.put(name,map.get(name) + value);
-                }
             }
-            Double max = map.values().stream().max(Comparator.naturalOrder()).get();
-            map.entrySet().stream().filter(entry -> entry.getValue().equals(max))
-                    .forEach(entry -> System.out.println(entry.getKey()));
         }
+        Double max = Collections.max(map.values());
+        ArrayList<String> name = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            if (entry.getValue().equals(max)) {
+                name.add(entry.getKey());
+            }
+        }
+        for (String s : name) {
+            System.out.println(s);
+        }
+    }
 
+    public static void main1(String[] args) throws IOException {
+        TreeMap<String, Double> map = Files.readAllLines(Path.of(args[0])).stream().map(s -> s.split(" ")).
+                collect(Collectors.toMap(strings -> String.valueOf(strings[0]), strings -> Double.parseDouble(strings[1]), Double::sum, TreeMap::new));
+        map.entrySet().stream().filter(entry -> entry.getValue().equals(Collections.max(map.values())))
+                .forEach(entry -> System.out.println(entry.getKey()));
     }
 }
+
+
