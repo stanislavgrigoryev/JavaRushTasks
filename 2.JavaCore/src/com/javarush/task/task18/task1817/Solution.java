5695ce262f0f457ae2b257c1bbe769e4a38aa037
setka <setka1748@mail.ru>
2024-01-22 15:04:35 +0500

    JavaRushCore task1817

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task18.task1817;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/* 
+Пробелы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int total = 0;
+        int spaces = 0;
+        try (FileReader fileReader = new FileReader(args[0])) {
+            while (fileReader.ready()) {
+                int readedChar = fileReader.read();
+                total++;
+                if (readedChar == (int) ' ') spaces++;
+            }
+        }
+        if (total != 0) {
+            double result = (double) spaces / total * 100;
+            System.out.printf("%.2f", result);
+        }
+    }
+}
