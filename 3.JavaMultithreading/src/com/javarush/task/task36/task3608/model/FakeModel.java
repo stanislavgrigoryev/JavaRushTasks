35e41587cd5178c5e532d400d31da6ded0d5c223
setka <setka1748@mail.ru>
2024-02-11 16:21:14 +0500

    JavaRushMultithreading task3608

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", 1, 12));
+        users.add(new User("Kolya", 2, 8));
+        modelData.setUsers(users);
+    }
+}
