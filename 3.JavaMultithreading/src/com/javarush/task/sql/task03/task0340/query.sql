1b231e2d46628296870f52aa14527630002283f3
setka <setka1748@mail.ru>
2024-03-11 22:08:03 +0500

    JavaRushSQL 0340/41

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0340/query.sql
@@ -0,0 +1,1 @@
+select * from authors where id not in (select author_id from books where author_id is not null and genre = 'novel');
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0341/query.sql
@@ -0,0 +1,1 @@
+select * from films where title like (select title from films where title like 'The%r' limit 1);
