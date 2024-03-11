19d8134fcf6b9599160ca2474111270e05f30e9f
setka <setka1748@mail.ru>
2024-03-11 14:58:25 +0500

    JavaRushSQL 0335

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
@@ -0,0 +1,5 @@
+select author.country as author_country, count(book.book_id) as book_count
+from authors as author JOIN books as book on author.id = book.author_id
+where book.genre = 'novel'
+group by author.country
+having count(book.book_id) > 2;
