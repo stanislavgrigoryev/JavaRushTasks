1dfcf5beaffe5e01b33470ad5541c7703132de55
setka <setka1748@mail.ru>
2024-03-24 12:29:33 +0500

    JavaRushSQL 0604

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE 'users'
+(
+    'user_id'    INT NOT NULL AUTO_INCREMENT,
+    'first_name' VARCHAR(255) NOT NULL,
+    'last_name'  VARCHAR(255) NOT NULL,
+        PRIMARY KEY(user_id)
+);
