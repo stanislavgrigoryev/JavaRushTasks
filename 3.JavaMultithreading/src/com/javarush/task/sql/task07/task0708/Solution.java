4c8ce2a69b5a64d469f128d0084e52515cbbb7a9
setka <setka1748@mail.ru>
2024-03-26 14:33:34 +0500

    JavaRushSQL 0708

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0708/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.sql.task07.task0708;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+/* 
+Получение даты
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root","root");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name, created FROM employee");
+        while (resultSet.next()){
+            String name = resultSet.getString("name");
+            LocalDateTime created = resultSet.getObject("created", LocalDateTime.class);
+            System.out.println(name + " " + created);
+        }
+        connection.close();
+        statement.close();
+    }
+}
