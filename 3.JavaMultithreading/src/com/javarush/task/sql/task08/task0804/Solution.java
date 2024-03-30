6b5cdeebbe98c1a6dc75eccb9ebae4d28159e071
setka <setka1748@mail.ru>
2024-03-30 14:52:04 +0500

    JavaRushSQL 0804

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0804/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.sql.task08.task0804;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/* 
+task0804
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        String sql = "insert into employee (name, age, smth) values (?, ?, ?)";
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < 5; i++) {
+            preparedStatement.setString(1, "employee_" + i);
+            preparedStatement.setInt(2, 30 + i);
+            preparedStatement.setString(3, "i = " + i);
+            preparedStatement.addBatch();
+        }
+        int[] ints = preparedStatement.executeBatch();
+        connection.close();
+        preparedStatement.close();
+    }
+}
