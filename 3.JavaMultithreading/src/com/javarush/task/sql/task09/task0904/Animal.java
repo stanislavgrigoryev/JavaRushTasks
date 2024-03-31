184ee6ef450c5e232d92467aba06501286c10b79
setka <setka1748@mail.ru>
2024-03-31 14:17:43 +0500

    JavaRushSQL 0904

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0904/Animal.java
@@ -0,0 +1,52 @@
+package com.javarush.task.sql.task09.task0904;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "animal_table", schema = "test")
+public class Animal {
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "family")
+    private String family;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+}
