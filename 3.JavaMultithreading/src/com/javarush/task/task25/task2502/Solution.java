069ed82a2346c7814e2f1c6d77f342b01b351df7
setka <setka1748@mail.ru>
2024-04-05 15:29:10 +0500

    JavaRushMultithreading2502

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Машину на СТО не повезем!
+*/
+
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            wheels = new ArrayList<>();
+            if (loadWheelNamesFromDB() == null) {
+                throw new NullPointerException();
+            }
+            if (loadWheelNamesFromDB().length < 4) {
+                throw new RuntimeException();
+            }
+            if (loadWheelNamesFromDB().length > 4) {
+                throw new RuntimeException();
+            }
+
+            for (String s : loadWheelNamesFromDB()) {
+                wheels.add(Wheel.valueOf(s));
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
