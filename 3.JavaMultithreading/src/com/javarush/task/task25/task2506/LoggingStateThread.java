359795e6d44f4fb4edbce834e6a20c93470b0bc0
setka <setka1748@mail.ru>
2024-04-07 15:59:59 +0500

    JavaRushMultithreading2506

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
@@ -4,11 +4,25 @@ public class LoggingStateThread extends Thread {
     Thread target;
 
     public LoggingStateThread(Thread target) {
+
         this.target = target;
+        this.setDaemon(true);
     }
 
     @Override
     public void run() {
 
+        State state = target.getState();
+        System.out.println(state);
+        while (true){
+            State newState = target.getState();
+            if (newState!=state){
+                System.out.println(newState);
+                state = newState;
+            }else if (newState == State.TERMINATED){
+                break;
+
+            }
+        }
     }
 }
