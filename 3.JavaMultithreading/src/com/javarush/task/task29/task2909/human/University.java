dc41136ffdef189b1a5e71e9846908a2b5381f11
setka <setka1748@mail.ru>
2024-04-07 17:11:49 +0500

    JavaRushMultithreading2509/6

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2909.human;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class University {
@@ -8,6 +9,11 @@ public class University {
     private String name;
     private int age;
     private List<Student> students = new ArrayList<>();
+
+    public University(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;
@@ -33,21 +39,22 @@ public class University {
         this.students = students;
     }
 
-    public University(String name, int age) {
 
+    public Student getStudentWithAverageGrade(double averageGrade) {
+
+        return students.stream().filter(student -> student.getAverageGrade() == averageGrade).findFirst().get();
     }
 
-    public Student getStudentWithAverageGrade() {
-        //TODO:
-        return null;
+    public Student getStudentWithMaxAverageGrade() {
+
+        return students.stream().max(Comparator.comparingDouble(Student::getAverageGrade)).get();
     }
 
-    public Student getStudentWithMaxAverageGrade(double averageGrade) {
-        //TODO:
-        return null;
+    public Student getStudentWithMinAverageGrade() {
+        return students.stream().min(Comparator.comparingDouble(Student::getAverageGrade)).get();
     }
 
-    public void getStudentWithMinAverageGradeAndExpel() {
-        //TODO:
+    public void expel(Student student) {
+        students.remove(student);
     }
 }
