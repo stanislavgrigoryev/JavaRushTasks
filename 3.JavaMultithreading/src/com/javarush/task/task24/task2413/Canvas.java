27a8ad1d503056e11e2e55aae19cb42d7cf8e1ef
setka <setka1748@mail.ru>
2024-04-14 20:56:17 +0500

    JavaRushMultithreading2413

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -8,7 +8,7 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.matrix = new char[height+2][width+2];
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public int getWidth() {
@@ -33,5 +33,21 @@ public class Canvas {
 
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
+    }
+
+   public void setPoint(double x, double y, char c) {
+        if (!(x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length)) {
+            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
     }
 }
