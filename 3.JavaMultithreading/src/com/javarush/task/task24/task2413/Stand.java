956c9fdadd4963a402ad3c4c22073ca81c29f03a
setka <setka1748@mail.ru>
2024-04-19 15:28:14 +0500

    JavaRushMultithreading 2413

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,20 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
     }
 
     @Override
