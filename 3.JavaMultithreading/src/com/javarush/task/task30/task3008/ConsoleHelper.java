cde695d0677ee62d43430324a7eac9d5bfabeea9
setka <setka1748@mail.ru>
2024-04-28 13:10:30 +0500

    JavaRushMultithreading 3008/2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,37 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.*;
+
 public class ConsoleHelper {
+    private static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        String read;
+
+        while (true) {
+            try {
+                read = console.readLine();
+                break;
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return read;
+    }
+
+    public static int readInt() {
+        int number;
+        try {
+            number = Integer.parseInt(readString());
+        } catch (NumberFormatException | IOException e) {
+            writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            number = readInt();
+        }
+
+        return number;
+    }
 }
