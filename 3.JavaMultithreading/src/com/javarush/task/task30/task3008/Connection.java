51975b0fd93870445a547787de1415f9f7bc2a2e
setka <setka1748@mail.ru>
2024-04-28 20:11:39 +0500

    JavaRushMultithreading 3008

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,4 +1,44 @@
 package com.javarush.task.task30.task3008;
 
-public class Connection {
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectInputStream in;
+    private final ObjectOutputStream out;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        Message read;
+        synchronized (in) {
+            read = (Message) in.readObject();
+        }
+        return read;
+
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+        in.close();
+        out.close();
+
+    }
 }
