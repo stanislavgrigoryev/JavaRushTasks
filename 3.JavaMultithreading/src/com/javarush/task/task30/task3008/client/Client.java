194c2e2fb7c194bd44b3851b8f82c265f2310cdd
setka <setka1748@mail.ru>
2024-05-01 13:40:03 +0500

    JavaRushMultithreading 3008/14

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -7,24 +7,22 @@ import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
 
+
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
-    protected String getServerAddress() throws IOException{
-        ConsoleHelper.writeMessage("Введите адрес сервера:");
+    protected String getServerAddress() throws IOException {
         String address = ConsoleHelper.readString();
         return address;
     }
 
     protected int getServerPort() {
-        ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
         return port;
     }
 
-    protected String getUserName() throws IOException{
-        ConsoleHelper.writeMessage("Введите ваше имя:");
+    protected String getUserName() throws IOException {
         String name = ConsoleHelper.readString();
         return name;
     }
@@ -34,18 +32,58 @@ public class Client {
     }
 
     protected SocketThread getSocketThread() {
-        return new SocketThread();
+        SocketThread socketThread = new SocketThread();
+        return socketThread;
     }
 
     protected void sendTextMessage(String text) throws IOException, ClassNotFoundException {
         try {
             connection.send(new Message(MessageType.TEXT, text));
         } catch (IOException e) {
-            ConsoleHelper.writeMessage("Произошла ошибка. Не удалось отправить сообщение");
+            ConsoleHelper.writeMessage("Произошла ошибка");
             clientConnected = false;
         }
     }
 
+    public void run() throws IOException, ClassNotFoundException {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                wait();
+                this.notify(); //?
+                if (clientConnected) {
+                    ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+                } else {
+                    ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+                }
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Произошла ошибка");
+                socketThread.interrupt();
+            }
+            while (clientConnected) {
+                String message = ConsoleHelper.readString();
+                if (message.equals("exit")) {
+                    break;
+                }
+                if (shouldSendTextFromConsole()) {
+                    sendTextMessage(message);
+                }
+
+            }
+        }
+
+    }
+
+
     public class SocketThread extends Thread {
+
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Client client = new Client();
+        client.run();
+
     }
 }
