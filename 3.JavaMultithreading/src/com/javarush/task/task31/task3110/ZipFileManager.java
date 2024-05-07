840ba50894649ac2087cb38539a1c01316718e5f
setka <setka1748@mail.ru>
2024-05-07 11:35:38 +0500

    JavaRushMultithreading 3110

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.File;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -9,7 +14,9 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
+        OutputStream zipFile1 = Files.newOutputStream(zipFile);
+
 
     }
 }
