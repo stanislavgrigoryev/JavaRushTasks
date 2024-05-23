eb93a99633eaa671203fb31278ba446465a44742
setka <setka1748@mail.ru>
2024-05-23 15:38:17 +0500

    JavaRushMultithreading 2712

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,4 +1,12 @@
 package com.javarush.task.task27.task2712.ad;
 
-public class AdvertisementManager{
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdvertisementManager {
+
+    final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+
+    public AdvertisementManager() {
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,14 +1,29 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
+    private final List<Advertisement> videos = new ArrayList<>();
     private static AdvertisementStorage instance;
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60));
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60));
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));
 
     }
     public static synchronized AdvertisementStorage getInstance(){
         if (instance == null){
             instance = new AdvertisementStorage();
         } return instance;
+    }
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
