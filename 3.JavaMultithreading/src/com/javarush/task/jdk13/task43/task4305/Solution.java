b753f711ff83c9437ef959ef702ab704cef67e4f
setka <setka1748@mail.ru>
2024-05-28 13:11:16 +0500

    JavaRushMultithreading 4305

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4305/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.jdk13.task43.task4305;
+
+/* 
+Вернуться к истокам
+*/
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+        boolean result = helloWorldChecker(choice);
+        System.out.println("The result is: " + result);
+    }
+
+    public static boolean helloWorldChecker(String s) {
+        boolean b = StringUtils.containsOnly(s, "Helo Wrd!");
+        return b;
+    }
+}
