597ccba1ff4261c3d8edc6718d8a09f398a5a437
setka <setka1748@mail.ru>
2024-06-10 15:22:11 +0500

    JavaRushMultithreading 4108

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4108/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task41.task4108;
+
+//import com.javarush.task.jdk13.task41.task4108.factories.JavaRush;
+
+/* 
+Фабрики кадров
+*/
+
+import com.javarush.task.jdk13.task41.task4108.factories.JavaRush;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Team javaTeam = new Team(new JavaRush());
+        javaTeam.hireSeniors(1);
+        javaTeam.hireMiddles(3);
+        javaTeam.hireJuniors(8);
+        javaTeam.produceCode();
+    }
+}
