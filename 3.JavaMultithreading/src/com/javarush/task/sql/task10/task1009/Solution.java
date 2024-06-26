39d06d7dfaf7606bb75aba2c7d8809683aed41ec
setka <setka1748@mail.ru>
2024-06-26 16:39:44 +0500

    JavaRushMultithreading 1009

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1009/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.sql.task10.task1009;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.stream.Collectors;
+
+/* 
+task1009
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Salary fund: $" + getSalaryFund());
+        System.out.println("Agerage age: " + getAverageAge());
+    }
+
+    public static Long getSalaryFund() {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
+            String hql = "SELECT SUM(e.salary) FROM Employee e";
+            Query<Long> query = session.createQuery(hql, Long.class);
+            return query.getSingleResult();
+        }
+    }
+
+    public static Double getAverageAge() {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
+            String hql = "SELECT AVG(e.age) FROM Employee e";
+            Query<Double> query = session.createQuery(hql, Double.class);
+            return query.getSingleResult();
+        }
+    }
+}
