cb495c178673b69ed6baea86c81c8473cdde95cd
setka <setka1748@mail.ru>
2024-06-28 13:55:01 +0500

    JavaRushMultithreading 4121

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4121/CRM.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4121/CRM.java
@@ -1,14 +1,21 @@
 package com.javarush.task.jdk13.task41.task4121;
 
-import com.javarush.task.jdk13.task41.task4121.support.TechSupport;
+import com.javarush.task.jdk13.task41.task4121.support.*;
 
 public class CRM {
 
     private TechSupport techSupport;
 
     public CRM() {
+        techSupport = new CallCenter();
+        HelpDesk helpDesk = new HelpDesk();
+        SysAdmin sysAdmin = new SysAdmin();
+        ITDirector itDirector = new ITDirector();
+        techSupport.setNextLevel(helpDesk);
+        helpDesk.setNextLevel(sysAdmin);
+        sysAdmin.setNextLevel(itDirector);
 
-        this.techSupport = new TechSupport();
+
     }
 
     public void register(String problem) {
